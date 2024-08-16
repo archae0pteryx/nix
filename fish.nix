ca9ac70bb8cedb59260bdf7cf97b1a3bace1129e
archae0pteryx <github@pocketcereal.com>
2024-08-16 09:50:46 -0700

    separate fish

---
+++ fish.nix
@@ -0,0 +1,47 @@
+{
+  enable = true;
+  shellAliases = {
+    rebuild = "home-manager switch --flake $HOME/.config/home-manager";
+    opensource = "cd $HOME/Code/opensource";
+    eyepop = "cd $HOME/Code/eyepop";
+    k = "kubectl";
+    kc = "kubectl config";
+    kns = "kubens";
+    kx = "kubectx";
+    tf = "opentofu";
+    tfa = "opentofu apply";
+    tfd = "opentofu destroy";
+    tfaa = "opentofu apply --auto-approve";
+    tg = "terragrunt";
+    tga = "terragrunt apply";
+    tgp = "terragrunt plan";
+    tgd = "terragrunt destroy";
+    tgaa = "terragrunt apply --auto-approve";
+    ".." = "cd ..";
+    "..." = "cd ../..";
+    "...." = "cd ../../..";
+    dc = "docker compose";
+    dcb = "docker compose build";
+    dcd = "docker compose down";
+    nixconfig = "code $HOME/.config/home-manager";
+    alacrittyconfig = "code $HOME/.config/alacritty";
+    c = "cd $HOME/Code";
+    ga = "git add";
+    gc = "git commit -S -m";
+    gp = "git push";
+    gco = "git checkout";
+    nixupdate =
+      "sudo -i nix-channel --update && sudo -i nixos-rebuild switch && home-manager switch --flake ~/.config/home-manager";
+    nixclean =
+      "sudo -i nix-collect-garbage -di && sudo -i nix-store --optimise";
+    starshipconfig = "code $HOME/.config/starship.toml";
+  };
+  shellInit = ''
+    if status --is-interactive
+        if not set -q TMUX
+            tmux new-session
+        end
+        starship init fish | source
+    end
+  '';
+}
--- programs.nix
+++ programs.nix
@@ -5,51 +5,52 @@
     enable = true;
     forwardAgent = true;
   };
-  fish = {
-    enable = true;
-    shellAliases = {
-      rebuild = "home-manager switch --flake $HOME/.config/home-manager";
-      opensource = "cd $HOME/Code/opensource";
-      eyepop = "cd $HOME/Code/eyepop";
-      k = "kubectl";
-      kc = "kubectl config";
-      kns = "kubens";
-      kx = "kubectx";
-      tf = "opentofu";
-      tfa = "opentofu apply";
-      tfd = "opentofu destroy";
-      tfaa = "opentofu apply --auto-approve";
-      tg = "terragrunt";
-      tga = "terragrunt apply";
-			tgp = "terragrunt plan";
-      tgd = "terragrunt destroy";
-      tgaa = "terragrunt apply --auto-approve";
-      ".." = "cd ..";
-      "..." = "cd ../..";
-      "...." = "cd ../../..";
-      dc = "docker compose";
-      dcb = "docker compose build";
-      dcd = "docker compose down";
-      nixconfig =
-        "code $HOME/.config/home-manager";
-      c = "cd $HOME/Code";
-      ga = "git add";
-      gc = "git commit -S -m";
-      gp = "git push";
-      gco = "git checkout";
-      nixupdate = "sudo -i nix-channel --update && sudo -i nixos-rebuild switch && home-manager switch --flake ~/.config/home-manager";
-      nixclean = "sudo -i nix-collect-garbage -di && sudo -i nix-store --optimise";
-      starshipconfig = "code $HOME/.config/starship.toml";
-    };
-    shellInit = ''
-if status --is-interactive
-    if not set -q TMUX
-        tmux new-session
-    end
-    starship init fish | source
-end
-    '';
-  };
+#   fish = {
+#     enable = true;
+#     shellAliases = {
+#       rebuild = "home-manager switch --flake $HOME/.config/home-manager";
+#       opensource = "cd $HOME/Code/opensource";
+#       eyepop = "cd $HOME/Code/eyepop";
+#       k = "kubectl";
+#       kc = "kubectl config";
+#       kns = "kubens";
+#       kx = "kubectx";
+#       tf = "opentofu";
+#       tfa = "opentofu apply";
+#       tfd = "opentofu destroy";
+#       tfaa = "opentofu apply --auto-approve";
+#       tg = "terragrunt";
+#       tga = "terragrunt apply";
+# 			tgp = "terragrunt plan";
+#       tgd = "terragrunt destroy";
+#       tgaa = "terragrunt apply --auto-approve";
+#       ".." = "cd ..";
+#       "..." = "cd ../..";
+#       "...." = "cd ../../..";
+#       dc = "docker compose";
+#       dcb = "docker compose build";
+#       dcd = "docker compose down";
+#       nixconfig =
+#         "code $HOME/.config/home-manager";
+#       c = "cd $HOME/Code";
+#       ga = "git add";
+#       gc = "git commit -S -m";
+#       gp = "git push";
+#       gco = "git checkout";
+#       nixupdate = "sudo -i nix-channel --update && sudo -i nixos-rebuild switch && home-manager switch --flake ~/.config/home-manager";
+#       nixclean = "sudo -i nix-collect-garbage -di && sudo -i nix-store --optimise";
+#       starshipconfig = "code $HOME/.config/starship.toml";
+#     };
+#     shellInit = ''
+# if status --is-interactive
+#     if not set -q TMUX
+#         tmux new-session
+#     end
+#     starship init fish | source
+# end
+#     '';
+#   };
+  fish = import ./fish.nix;
   go = {
     enable = true;
     goPath = "/home/rimraf/Code/go";
