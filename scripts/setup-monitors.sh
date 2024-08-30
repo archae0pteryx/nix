8c9bbd0261fd5aba8950918987b8dfe14a083201
archae0pteryx <github@pocketcereal.com>
2024-08-30 07:07:58 -0700

    fix monitor script

--- scripts/setup-monitors.sh
+++ scripts/setup-monitors.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-xrandr --output HDMI-3 --mode 1920x1080 --primary
-xrandr --output HDMI-2 --mode 1920x1080 --right-of HDMI-3
+xrandr --output HDMI-2 --mode 1920x1080 --primary
+xrandr --output HDMI-1 --mode 1920x1080 --right-of HDMI-2
 
