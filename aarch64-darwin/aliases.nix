f58251802aa876298564bf565da7e5ca24ab7462
archae0pteryx <github@pocketcereal.com>
2024-09-23 07:13:17 -0700

    updates

--- aarch64-darwin/aliases.nix
+++ aarch64-darwin/aliases.nix
@@ -4,7 +4,6 @@ let
     nixconfig = "code $HOME/.config/nix";
     rebuild = "darwin-rebuild switch --flake $HOME/.config/nix/aarch64-darwin";
     hammerconfig = "code $HOME/.hammerspoon";
-    
   };
 in
 {
