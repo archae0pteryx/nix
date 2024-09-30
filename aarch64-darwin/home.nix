3cc682e0da89026b69c39945c1cbf2c2e991b116
archae0pteryx <github@pocketcereal.com>
2024-09-30 08:50:09 -0700

    dfisable amethy

--- aarch64-darwin/home.nix
+++ aarch64-darwin/home.nix
@@ -6,15 +6,15 @@ in {
   home.stateVersion = "24.11";
   home.file = { ".vimrc".source = ../common/vimrc; };
   home.file = { ".tmux.conf".source = ../common/tmux.conf; };
-
+‹
   xdg.configFile."starship.toml".text =
     builtins.readFile ../common/starship.toml;
   xdg.configFile."alacritty/alacritty.toml".text =
     builtins.readFile ../common/alacritty.toml;
   xdg.configFile."fish/completions/devbox".text =
     builtins.readFile ../common/fish/completions/devbox;
-  xdg.configFile."amethyst/amethyst.yml".text =
-    builtins.readFile ../common/amethyst.yml;
+  # xdg.configFile."amethyst/amethyst.yml".text =
+  #   builtins.readFile ../common/amethyst.yml;
   home.sessionVariables = { EDITOR = "vim"; };
   home.packages = [
     (pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; })
