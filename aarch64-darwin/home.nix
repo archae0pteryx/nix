c591cce555ad2b5712c9c3cc820d880f94e25eaf
archae0pteryx <github@pocketcereal.com>
2024-09-30 08:50:41 -0700

    fix typo

--- aarch64-darwin/home.nix
+++ aarch64-darwin/home.nix
@@ -6,7 +6,7 @@ in {
   home.stateVersion = "24.11";
   home.file = { ".vimrc".source = ../common/vimrc; };
   home.file = { ".tmux.conf".source = ../common/tmux.conf; };
-‹
+
   xdg.configFile."starship.toml".text =
     builtins.readFile ../common/starship.toml;
   xdg.configFile."alacritty/alacritty.toml".text =
