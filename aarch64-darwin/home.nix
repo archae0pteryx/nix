80f5923d426cb62ab1d7c1956f3eb66f6d1c02a2
archae0pteryx <github@pocketcereal.com>
2024-11-03 07:46:43 -0800

    update starship

--- aarch64-darwin/home.nix
+++ aarch64-darwin/home.nix
@@ -7,8 +7,8 @@ in {
   home.file = { ".vimrc".source = ../common/vimrc; };
   home.file = { ".tmux.conf".source = ../common/tmux.conf; };
 
-  # xdg.configFile."starship.toml".text =
-  #   builtins.readFile ../common/starship.toml;
+  xdg.configFile."starship.toml".text =
+    builtins.readFile ../common/starship.toml;
   xdg.configFile."alacritty/alacritty.toml".text =
     builtins.readFile ../common/alacritty.toml;
   xdg.configFile."fish/completions/devbox".text =
