0f1d8a5a7e72a0b56b6cda6dbc2e632a215fd155
archae0pteryx <github@pocketcereal.com>
2024-11-20 10:29:56 -0800

    update linux aliases

--- linux/nixos/extra-aliases.nix
+++ linux/nixos/extra-aliases.nix
@@ -1,14 +1,7 @@
-{}:
-let
-  shellAliases = {
+{
     rebuild = "home-manager switch --flake $HOME/.config/nix/linux/nixos";
     nixupdate =
       "sudo -i nix-channel --update && sudo -i nixos-rebuild switch && home-manager switch --flake $HOME/.config/nix/linux/nixos";
     nixclean = "sudo -i nix-collect-garbage -d && sudo -i nix-store --optimise";
     setmonitors = "$HOME/.config/nix/linux/nixos/scripts/setup-monitors.sh";
-  };
-
-in
-{
-  inherit shellAliases;
 }
