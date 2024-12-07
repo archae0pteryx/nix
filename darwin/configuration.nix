92c03c9a83e841ad98f4cbecc8e216d980b79eee
archae0pteryx <github@pocketcereal.com>
2024-12-07 06:37:20 -0800

    claire update

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -40,22 +40,22 @@ in {
 
   programs.zsh.enable = true;
   programs.bash.enable = true;
-
-  users.users.${systemUser} = {
-    name = "${systemUser}";
-    home = "/Users/${systemUser}";
-    shell = pkgs.zsh;
-    openssh.authorizedKeys.keyFiles = [ 
-      ../common/ssh/claire.local.pub
-      ../common/ssh/eyepop.local.pub
-      ../common/ssh/nixos.local.pub
-    ];
+  users = {
+    groups.docker = { members = [ "${systemUser}" ]; };
+    users.${systemUser} = {
+      name = "${systemUser}";
+      home = "/Users/${systemUser}";
+      shell = pkgs.zsh;
+      openssh.authorizedKeys.keyFiles = [
+        ../common/ssh/claire.local.pub
+        ../common/ssh/eyepop.local.pub
+        ../common/ssh/nixos.local.pub
+      ];
+    };
   };
 
   homebrew = import ../common/homebrew.nix;
-  system = import ./system-prefs.nix {
-    inherit hostname;
-  };
+  system = import ./system-prefs.nix { inherit hostname; };
   home-manager = {
     useGlobalPkgs = true;
     useUserPackages = true;
