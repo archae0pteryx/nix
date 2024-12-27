3f4e2242112a418d4e6e9ba220ecdc6fcc6738af
archae0pteryx <github@pocketcereal.com>
2024-12-26 16:10:24 -0800

    update tmux script

--- darwin/zsh.nix
+++ darwin/zsh.nix
@@ -7,37 +7,30 @@
   antidote.plugins = [ "agkozak/zsh-z" ];
   shellAliases = import ./${hostname}/extra-aliases.nix;
   syntaxHighlighting.enable = true;
-  envExtra = ''
+  initExtra = ''
     . "$HOME/.cargo/env"
     source <(fzf --zsh)
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
     [ -s "$NVM_DIR/zsh_completion" ] && \. "$NVM_DIR/zsh_completion"
-  '';
-  initExtra = ''
-  manage_tmux_session() {
-      if ! command -v tmux &> /dev/null; then
-          echo "tmux is not installed. Please install tmux to use this feature."
-          return
-      fi
+    manage_tmux_session() {
+        if ! command -v tmux &> /dev/null; then
+            echo "tmux is not installed. Please install tmux to use this feature."
+            return
+        fi
 
-      if [ -z "$TMUX" ]; then
-          SESSION="foobar"
-          if tmux has-session -t "$SESSION" 2>/dev/null; then
-              tmux attach-session -t "$SESSION" \; new-window
-          else
-              tmux new-session -s "$SESSION"
-          fi
-      fi
-  }
+        if [ -z "$TMUX" ]; then
+            SESSION="foobar"
+            tmux new-session -s "$SESSION"
+        fi
+    }
 
-  # Only in interactive mode
-  case $- in
-      *i*)
-          manage_tmux_session
-          ;;
-      *)
-          ;;
-  esac
-  
+    case $- in
+        *i*)
+            manage_tmux_session
+            ;;
+        *)
+            ;;
+    esac
+
   '';
 }
