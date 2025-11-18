23cb9bc0915ff81eff66a286c0bca783c744f9eb
Ryan <github@pocketcereal.com>
2025-11-18 13:17:09 -0800

    save

--- darwin/eyepop/extra-env.nix
+++ darwin/eyepop/extra-env.nix
@@ -1,5 +1,5 @@
 ''
-  export LDFLAGS="-L/opt/homebrew/lib"
+  export LDFLAGS="-L/opt/homebrew/lib -L/opt/homebrew/opt/libpq/lib"
   export CPPFLAGS="-I/opt/homebrew/include"
   export PKG_CONFIG_PATH="/opt/homebrew/lib/pkgconfig:/opt/homebrew/Library/Homebrew/os/mac/pkgconfig:$PKG_CONFIG_PATH"
   export KUBECTL_EXTERNAL_DIFF="colordiff -N -u"
